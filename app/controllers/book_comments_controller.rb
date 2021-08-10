da050ed6a49a94dbfd0b4060acdb0be75532a5de
takuminemoto <nemoto1671055@gmail.com>
2021-08-10 02:32:18 +0000

    課題6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,15 +1,20 @@
 class BookCommentsController < ApplicationController
+  before_action :authenticate_user!
+  
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.post_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    @book_comment.user_id = current_user.id
+    unless @book_comment.save
+      render 'error'
+    end
   end
 
   def destroy
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
+    @book = Book.find(params[:book_id])
+    book_comment = @book.book_comments.find(params[:id])
+    book_comment.destroy
   end
   
   private
